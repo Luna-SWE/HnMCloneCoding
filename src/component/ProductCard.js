0ee18f869518482eff5eb19e0f1baa3556e46709
Luna <hyeyoung.luna.lee@gmail.com>
2024-08-23 22:57:26 +0800

    💄 Style: Change effect

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 const ProductCard = ({ item }) => {
   return (
-    <div>
-      <img
-        src={item?.img}
-        alt='test-img'
-        className='img-size-change'
-        width={230}
-      />
-      <div>{item?.choice === true ? 'Concious choice' : ''}</div>
+    <div className='img-size-change'>
+      <img src={item?.img} alt='test-img' width={230} />
+      <div>{item?.choice === true ? 'Concious Choice' : ''}</div>
       <div>{item?.title}</div>
       <div>{item?.price}</div>
       <div>{item?.new === true ? '신제품' : ''}</div>
